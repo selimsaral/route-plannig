0e00059283b410c5cf74f0271f2594e881bb36f4
selimsaral <saral_selim@hotmail.com>
2020-12-06 19:23:23 +0300

    Add Google Helper Files

---
+++ app/Helpers/GetLocations.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Jobs\LocationJob;
+
+class GetLocations
+{
+    private array $addresses = [];
+    private array $locations = [];
+
+    public function setAddress(string $address): GetLocations
+    {
+        $this->addresses[] = $address;
+
+        return $this;
+    }
+
+    public function setAddressList(array $addresses)
+    {
+        foreach ($addresses as $address) {
+            $this->addresses[] = $address;
+        }
+
+        return $this;
+    }
+
+    public function getLocations(): array
+    {
+        return $this->locations;
+    }
+
+    public function locationDetails(): GetLocations
+    {
+        foreach ($this->addresses as $address) {
+            $this->locations[] = LocationJob::dispatchNow($address);
+        }
+
+        return $this;
+    }
+}
